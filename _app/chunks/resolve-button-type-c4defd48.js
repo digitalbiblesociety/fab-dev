c8eb6443778c24f9bc70f93eaf4ddfbaa37b559a
jonbitgood <jon@dbs.org>
2022-05-31 20:38:05 +0000

    Deploying to gh-pages from @ digitalbiblesociety/fab-dev@542f7693d3b3776ea3ca83291c1b2a4f850d8d4f 🚀

---
+++ _app/chunks/resolve-button-type-c4defd48.js
@@ -0,0 +1,1 @@
+function o(t,u){var n;if(t.type)return t.type;let e=(n=t.as)!=null?n:"button";if(typeof e=="string"&&e.toLowerCase()==="button"||u instanceof HTMLButtonElement)return"button"}export{o as r};
